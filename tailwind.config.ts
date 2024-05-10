2ad29ce637f48e885383b59fa55253b613c08a3d
Efezino Idisi <enduranceidisi13@gmail.com>
2024-05-10 08:55:35 +0100

    chore: update dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,48 +1,67 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'home-desktop': "url('/home/background-home-desktop.jpg')",
-        'home-tablet': "url('/home/background-home-tablet.jpg')",
-        'home-mobile': "url('/home/background-home-mobile.jpg')",
-        'destination-desktop':
+        "home-desktop": "url('/home/background-home-desktop.jpg')",
+        "home-tablet": "url('/home/background-home-tablet.jpg')",
+        "home-mobile": "url('/home/background-home-mobile.jpg')",
+        "destination-desktop":
           "url('/destination/background-destination-desktop.jpg')",
-        'destination-tablet':
+        "destination-tablet":
           "url('/destination/background-destination-tablet.jpg')",
-        'destination-mobile':
+        "destination-mobile":
           "url('/destination/background-destination-mobile.jpg')",
 
-        'crew-desktop': "url('/crew/background-crew-desktop.jpg')",
-        'crew-tablet': "url('/crew/background-crew-tablet.jpg')",
-        'crew-mobile': "url('/crew/background-crew-mobile.jpg')",
-        'technology-desktop':
+        "crew-desktop": "url('/crew/background-crew-desktop.jpg')",
+        "crew-tablet": "url('/crew/background-crew-tablet.jpg')",
+        "crew-mobile": "url('/crew/background-crew-mobile.jpg')",
+        "technology-desktop":
           "url('/technology/background-technology-desktop.jpg')",
-        'technology-tablet':
+        "technology-tablet":
           "url('/technology/background-technology-tablet.jpg')",
-        'technology-mobile':
+        "technology-mobile":
           "url('/technology/background-technology-mobile.jpg')",
       },
       colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-        primary: 'var(--primary)',
-        nav: 'rgba(255, 255, 255, 0.04)',
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: "var(--primary)",
+        nav: "rgba(255, 255, 255, 0.04)",
       },
       backdropBlur: {
-        sl: '40.774227142333984px',
+        sl: "40.774227142333984px",
       },
 
       fontFamily: {
-        barlow: ['var(--font-barlow)'],
-        bellefair: ['var(--font-bellefair)'],
-        'barlow-condensed': ['var(--barlow-condensed)'],
+        barlow: ["var(--font-barlow)"],
+        bellefair: ["var(--font-bellefair)"],
+        "barlow-condensed": ["var(--barlow-condensed)"],
+      },
+
+      keyframes: {
+        "infinite-rotate": {
+          "0%": {
+            rotate: "0deg",
+          },
+
+          "50%": {
+            rotate: "180deg",
+          },
+          "100%": {
+            rotate: "360deg",
+          },
+        },
+      },
+
+      animation: {
+        "infinte-rotate": "infinite-rotate 10s infinite linear",
       },
     },
   },
